df38d829624c569c8a2d6c7e2593827510624f0b
ali akbar <ali.akbar@doaz.ai>
2024-07-18 03:22:06 +0500

    update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+# Set Ncloud credentials as environment variables
 export NCLOUD_ACCESS_KEY_ID=$NCLOUD_ACCESS_KEY
 export NCLOUD_ACCESS_SECRET_KEY=$NCLOUD_SECRET_KEY
 export NCLOUD_API_GW=https://ncloud.apigw.ntruss.com
 export NCLOUD_UUID=$NCLOUD_UUID
 export NCLOUD_REGION=$NCLOUD_REGION
 
+# Create Ncloud configuration file
 mkdir -p ~/.ncloud
 cat <<EOL > ~/.ncloud/configure
 [DEFAULT]
@@ -17,19 +19,19 @@ EOL
 # Update kubeconfig with ncp-iam-authenticator
 ncp-iam-authenticator update-kubeconfig --region $NCLOUD_REGION --clusterUuid $NCLOUD_UUID
 
-echo "Check KUBECONFIG---------------------a---"
+# Set KUBECONFIG environment variable
 export KUBECONFIG='~/.kube/config'
 
+# Check software versions
 echo "Check Software------------------------"
 aws --version
 kubectl version
 helm version
 echo "------------------------"
 
-# Helm Deployment
+# Construct Helm upgrade command
 UPGRADE_COMMAND="helm upgrade --install --timeout 30s"
-for config_file in ${DEPLOY_CONFIG_FILES}
-do
+for config_file in ${DEPLOY_CONFIG_FILES}; do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
 done
 if [ -n "$DEPLOY_NAMESPACE" ]; then
@@ -38,10 +40,11 @@ fi
 if [ -n "$DEPLOY_VALUES" ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
 fi
-
 if [ "$DRY_RUN" = true ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --dry-run"
 fi
 UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
+
+# Print and execute the Helm upgrade command
 echo "Executing: ${UPGRADE_COMMAND}"
 ${UPGRADE_COMMAND}
