e21267a334665d3adc0c02c9e6e6e3c101b0b837
ali akbar <ali.akbar@doaz.ai>
2024-07-18 03:41:05 +0500

    upddate for deploy.sh

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM python:3.10-rc-slim-buster
-WORKDIR /app
-COPY deploy.sh /app/deploy.sh
+WORKDIR /
+COPY deploy.sh /deploy.sh
 RUN apt update -y && apt install -y curl \
     && curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash \
     && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
     && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl \
     && curl -o ncp-iam-authenticator -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_linux_amd64 \
     && chmod +x ./ncp-iam-authenticator && mkdir -p /usr/local/bin && mv ./ncp-iam-authenticator /usr/local/bin/ncp-iam-authenticator \
-    && chmod +x /app/deploy.sh
-CMD ["bash", "./app/deploy.sh"]
+    && chmod +x /deploy.sh
+CMD ["bash", "./deploy.sh"]
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-
-# Set Ncloud credentials as environment variables
 export NCLOUD_ACCESS_KEY_ID=$NCLOUD_ACCESS_KEY
 export NCLOUD_ACCESS_SECRET_KEY=$NCLOUD_SECRET_KEY
 export NCLOUD_API_GW=https://ncloud.apigw.ntruss.com
 export NCLOUD_UUID=$NCLOUD_UUID
 export NCLOUD_REGION=$NCLOUD_REGION
-
-# Create Ncloud configuration file
 mkdir -p ~/.ncloud
 cat <<EOL > ~/.ncloud/configure
 [DEFAULT]
@@ -15,21 +11,13 @@ ncloud_access_key_id = $NCLOUD_ACCESS_KEY_ID
 ncloud_secret_access_key = $NCLOUD_ACCESS_SECRET_KEY
 ncloud_api_url = $NCLOUD_API_GW
 EOL
-
-# Update kubeconfig with ncp-iam-authenticator
 ncp-iam-authenticator update-kubeconfig --region $NCLOUD_REGION --clusterUuid $NCLOUD_UUID
-
-# Set KUBECONFIG environment variable
 export KUBECONFIG='~/.kube/config'
-
-# Check software versions
 echo "Check Software------------------------"
 aws --version
 kubectl version
 helm version
 echo "------------------------"
-
-# Construct Helm upgrade command
 UPGRADE_COMMAND="helm upgrade --install --timeout 30s"
 for config_file in ${DEPLOY_CONFIG_FILES}; do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
@@ -44,7 +32,5 @@ if [ "$DRY_RUN" = true ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --dry-run"
 fi
 UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
-
-# Print and execute the Helm upgrade command
 echo "Executing: ${UPGRADE_COMMAND}"
 ${UPGRADE_COMMAND}
