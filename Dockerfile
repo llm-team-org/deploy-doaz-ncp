f25927232679b04c7c9439d9b6fad0e74a3e52dc
ali akbar <ali.akbar@doaz.ai>
2024-07-19 22:06:37 +0500

    update for deployment NCP

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-FROM ubuntu:22.04
-WORKDIR /
-COPY deploy.sh /deploy.sh
-RUN apt update -y && apt install -y curl \
-    && apt-get install -y sudo \
-    && curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash \
-    && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
-    && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl \
-    && curl -o ncp-iam-authenticator -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_linux_amd64 \
-    && chmod +x ./ncp-iam-authenticator && mkdir -p /usr/local/bin && mv ./ncp-iam-authenticator /usr/local/bin/ncp-iam-authenticator \
-    && chmod +x /deploy.sh
-CMD ["bash", "./deploy.sh"]
+FROM python:3.12-rc-slim-buster
+
+COPY deploy.sh /usr/local/bin/deploy
+
+# Create the necessary directories
+RUN mkdir -p /root/.ncloud
+
+RUN apt -y update && apt -y install curl \
+    && curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash 
+
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+    && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl
+
+RUN curl -o ncp-iam-authenticator -L https://github.com/NaverCloudPlatform/ncp-iam-authenticator/releases/latest/download/ncp-iam-authenticator_linux_amd64 \
+    && chmod +x ./ncp-iam-authenticator && mkdir -p /usr/local/bin && mv ./ncp-iam-authenticator /usr/local/bin/ncp-iam-authenticator
+
+CMD deploy
--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,94 @@
 #!/bin/sh
-export NCLOUD_ACCESS_KEY_ID=$NCLOUD_ACCESS_KEY
-export NCLOUD_ACCESS_SECRET_KEY=$NCLOUD_SECRET_KEY
+
 export NCLOUD_API_GW=https://ncloud.apigw.ntruss.com
-export NCLOUD_UUID=$NCLOUD_UUID
-export NCLOUD_REGION=$NCLOUD_REGION
-mkdir -p ~/.ncloud
-cat <<EOF > ~/.ncloud/configure
+
+cat <<EOF >  /root/.ncloud/configure
 [DEFAULT]
-ncloud_access_key_id = $NCLOUD_ACCESS_KEY_ID
-ncloud_secret_access_key = $NCLOUD_ACCESS_SECRET_KEY
+ncloud_access_key_id = $NCLOUD_ACCESS_KEY
+ncloud_secret_access_key = $NCLOUD_SECRET_KEY
 ncloud_api_url = $NCLOUD_API_GW
+
 [project]
-ncloud_access_key_id = $NCLOUD_ACCESS_KEY_ID
-ncloud_secret_access_key = $NCLOUD_ACCESS_SECRET_KEY
+ncloud_access_key_id = $NCLOUD_ACCESS_KEY
+ncloud_secret_access_key = $NCLOUD_SECRET_KEY
 ncloud_api_url = $NCLOUD_API_GW
 EOF
 
-ncp-iam-authenticator update-kubeconfig --region $NCLOUD_REGION --clusterUuid $NCLOUD_UUID
-export KUBECONFIG='~/.kube/config'
+# Save Inital Path
+initial_path=$(pwd)
 
+
+# Create folder in home directory
+mkdir -p /home/kube
+echo "Kube DIR : " "/home/kube/config"
+cd /home/kube
+
+# Delete Config file if it exits
+file='config'
+if [ -f $file ]
+then 
+	echo "Removing $file"
+    	rm $file
+fi
+
+config=""
+echo "apiVersion: v1" > config
+echo "clusters:" >> config  
+echo "- cluster:" >> config
+echo "    certificate-authority-data: ${NCLOUD_CERT}" >> config
+echo "    server: ${NCLOUD_SERVER}" >> config 
+echo "  name: ${NCLOUD_NAME}" >> config  
+echo "contexts:" >> config  
+echo "- context:" >> config  
+echo "    cluster: ${NCLOUD_NAME}" >> config  
+echo "    user: ${NCLOUD_NAME}" >> config  
+echo "  name: ${NCLOUD_NAME}" >> config  
+echo "current-context: ${NCLOUD_NAME}" >> config  
+echo "kind: Config" >> config  
+echo "preferences: {}" >> config  
+echo "users:" >> config  
+echo "- name: ${NCLOUD_NAME}" >> config  
+echo "  user:" >> config  
+echo "    exec:" >> config  
+echo "      apiVersion: client.authentication.k8s.io/v1beta1" >> config  
+echo "      args:" >> config   
+echo "      - token" >> config  
+echo "      - --clusterUuid" >> config  
+echo "      - ${NCLOUD_UUID}" >> config
+echo "      - --region" >> config
+echo "      - ${NCLOUD_REGION}" >> config
+echo "      command: ncp-iam-authenticator" >> config  
+echo "      env: null" >> config   
+echo "      provideClusterInfo: false" >> config  
+
+chmod g-r config
+chmod o-r config
+cd ${initial_path}
+ls -al ${initial_path}
+
+echo "Check KUBECONFIG------------------------"
+export KUBECONFIG='/home/kube/config' 
 echo "Check Software------------------------"
 kubectl version
 helm version
 echo "------------------------"
 
+# Helm Deployment
 UPGRADE_COMMAND="helm upgrade --install --timeout 30s"
-
-for config_file in ${DEPLOY_CONFIG_FILES}; do
+for config_file in ${DEPLOY_CONFIG_FILES}
+do
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -f ${config_file}"
 done
-
 if [ -n "$DEPLOY_NAMESPACE" ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} -n ${DEPLOY_NAMESPACE}"
 fi
-
 if [ -n "$DEPLOY_VALUES" ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --set ${DEPLOY_VALUES}"
 fi
-
 if [ "$DRY_RUN" = true ]; then
     UPGRADE_COMMAND="${UPGRADE_COMMAND} --dry-run"
 fi
-
 UPGRADE_COMMAND="${UPGRADE_COMMAND} ${DEPLOY_NAME} ${DEPLOY_CHART_PATH}"
 echo "Executing: ${UPGRADE_COMMAND}"
 ${UPGRADE_COMMAND}
+rm -r /home/kube 
